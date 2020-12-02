3522f040ba15f713797413b91aad57acab401319
loganabc <c1520416296@126.com>
2020-12-02 21:43:47 +0800

    添加代码

---
+++ EurekaServer/src/main/java/com/work/eurekaserver/EurekaserverApplication8762.java
@@ -0,0 +1,15 @@
+package com.work.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaserverApplication8762 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaserverApplication8762.class, args);
+    }
+
+}
